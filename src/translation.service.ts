414fcead7ebf4da2836a91757278cb7ca38c8268
gangadharjannu <gangadhar014@gmail.com>
2020-02-20 17:14:21 +0100

    Feature 2: UX Improvements, added translations service

---
+++ src/translation.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+export class TranslationSet {
+  public languange: string
+  public values: { [key: string]: string } = {}
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TranslationService {
+
+  public languages = ['nl', 'en']
+
+  public language = 'en'
+
+  private dictionary: { [key: string]: TranslationSet } = {
+    nl: {
+      languange: 'nl',
+      values: {
+        'app.title': 'Produkten ING',
+        'app.product.details.title': 'product details',
+        'app.product.details.back': 'terug naar accounts'
+      },
+    },
+    en: {
+      languange: 'en',
+      values: {
+        'app.title': 'Products ING',
+        'app.product.details.title': 'productdetails',
+        'app.product.details.back': 'back to accounts'
+      },
+    },
+  }
+
+  constructor() { }
+
+  translate(key: string): string {
+    if (this.dictionary[this.language] != null) {
+      return this.dictionary[this.language].values[key]
+    }
+  }
+}
