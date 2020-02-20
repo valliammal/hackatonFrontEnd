7e78ad9c6094e3f45214c194faa507ca5f903517
gangadharjannu <gangadhar014@gmail.com>
2020-02-20 17:43:09 +0100

    TECHNICAL: fixed lint errors

--- src/app/account.service.ts
+++ src/app/account.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
@@ -13,21 +13,21 @@ export class AccountService {
     // return this.http.get<any>(`${environment.apiUrl}/accounts/{${userId}}`);
     // return this.httpClient.get<any>('http://dummy.restapiexample.com/api/v1/employees')
     return of({
-      "accounts": {
-        "savings": [
-          "product 1",
-          "product 2",
-          "product 3"
+      accounts: {
+        savings: [
+          'product 1',
+          'product 2',
+          'product 3'
         ],
-        "mortgage": [
-          "product 4",
-          "product 5",
-          "product 6"
+        mortgage: [
+          'product 4',
+          'product 5',
+          'product 6'
         ],
-        "payment": [
-          "product 7",
-          "product 8",
-          "product 9"
+        payment: [
+          'product 7',
+          'product 8',
+          'product 9'
         ]
       }
     });
